eee6a9e23a7bdd4cacfa82bca41b8c7422f2f30d
Yago Rodrigues <y.rodr1gues0811@gmail.com>
2023-11-14 17:09:07 -0300

    implementing CORS

--- 12_HATEOAS/rest-api/src/main/java/com/rodrigues/restapi/controllers/PersonController.java
+++ 12_HATEOAS/rest-api/src/main/java/com/rodrigues/restapi/controllers/PersonController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/*@CrossOrigin // Allows requests from other origins*/
 @RestController
 @RequestMapping("api/person/v1")
 @Tag(name = "People", description = "Endpoint for people's management")
@@ -23,6 +24,8 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
+
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(
             value = "/{id}",
             produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLCICATION_YAML}
@@ -68,27 +71,6 @@ public class PersonController {
         return ResponseEntity.ok().body(service.findAll());
     }
 
-    @PostMapping(
-            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLCICATION_YAML},
-            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLCICATION_YAML}
-    )
-    @Operation(
-            summary = "Adds a new person.", description = "Adds a new person by passing in a JSON, XML or YAML representation.",
-            tags = {"People"},
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Success: person created",
-                            content = @Content(schema = @Schema(implementation = PersonVOV1.class))
-                    ),
-                    @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-                    @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-                    @ApiResponse(responseCode = "404", description = "Not found", content = @Content),
-                    @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
-            }
-    )
-    public PersonVOV1 create(@RequestBody PersonVOV1 person) {
-        return service.create(person);
-    }
-
     @PutMapping(
             consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLCICATION_YAML},
             produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLCICATION_YAML}
@@ -107,6 +89,28 @@ public class PersonController {
     )
     public PersonVOV1 update(@RequestBody PersonVOV1 person) {
         return service.update(person);
+    }
+
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLCICATION_YAML},
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLCICATION_YAML}
+    )
+    @Operation(
+            summary = "Adds a new person.", description = "Adds a new person by passing in a JSON, XML or YAML representation.",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Success: person created",
+                            content = @Content(schema = @Schema(implementation = PersonVOV1.class))
+                    ),
+                    @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+                    @ApiResponse(responseCode = "404", description = "Not found", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
+            }
+    )
+    @CrossOrigin(origins = {"http://localhost:8080", "https://rodrigues.com.br"})
+    public PersonVOV1 create(@RequestBody PersonVOV1 person) {
+        return service.create(person);
     }
 
     @DeleteMapping(value = "/{id}")
