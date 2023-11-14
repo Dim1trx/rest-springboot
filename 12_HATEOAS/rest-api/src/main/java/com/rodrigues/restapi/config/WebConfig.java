8948809ea859c9ee87efacb3b1d27c2c39b2cc48
Yago Rodrigues <y.rodr1gues0811@gmail.com>
2023-11-14 17:21:35 -0300

    global CORS settings

--- 12_HATEOAS/rest-api/src/main/java/com/rodrigues/restapi/config/WebConfig.java
+++ 12_HATEOAS/rest-api/src/main/java/com/rodrigues/restapi/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.rodrigues.restapi.config;
 
 import com.rodrigues.restapi.serializationConverter.YamlJackson2HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,9 +15,27 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
     private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
 
+    //variables from application.yaml
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(", ");
+
+        registry.addMapping("/**")
+                /*permite a escolha de qual metodos sao permitidos
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")*/
+                .allowedMethods("*") // permite todos os metodos/verbos http
+                /*.allowedHeaders("*") // permite todos os headers;*/
+                .allowedOriginPatterns(allowedOrigins)
+                .allowCredentials(true);
+
     }
 
     @Override
